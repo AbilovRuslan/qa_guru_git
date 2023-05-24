bce7ddd9f377f64307b85de5ceea42c439faea4f
Abilov RU <ru_a@mail.ru>
2023-05-25 01:03:06 +0300

    var add

--- src/main/java/qa/guru/Main.java
+++ src/main/java/qa/guru/Main.java
@@ -4,9 +4,10 @@ package qa.guru;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        String message = "Hello  welcome!!!!!";
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello  welcome!!!!!");
+                System.out.println("message");
 
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
